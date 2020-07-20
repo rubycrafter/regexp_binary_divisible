c88112e85b5007a205633c1ba20c544a817c9715
antiko <Anton.Ikonnikov@krontech.ru>
2020-07-20 17:39:08 +0300

    WORKS!

--- Regular_Expression_for_Binary_Numbers_Divisible_by_n.rb
+++ Regular_Expression_for_Binary_Numbers_Divisible_by_n.rb
@@ -2,19 +2,13 @@
 
 # Main function, should return string
 def regex_divisible_by(n)
-  return '(0|1)*' if n == 1
+  return '^(0|1)*$' if n == 1
   fsm = FinitStateMachine.new(n)
- # p "FSM(#{n}): #{fsm.nodes}"
-
-  regexp = fsm.find_regexp
- # p regexp
-  regexp
+  fsm.find_regexp
 end
 
 
 class FinitStateMachine
-  attr_reader :nodes
-
   def initialize(divider)
     @n = divider
     # node index is FSM state
@@ -30,21 +24,20 @@ class FinitStateMachine
   end
 
   def remove_state(state)
-   # p "Current state: #{state}"
-   # p "Current FSM: #{@nodes}"
-
-    # creating array of all clouseres of state
+    # creating array of all closures of state
     closure_transitions = @nodes[state].find_all{ |tr, st| st == state }.map!(&:first)
 
     # creating full closure of state
-    closure = closure_transitions.empty? ? '' : "(#{closure_transitions.join('|')})*"
-
-   # p "Closure is: #{closure}"
+    closure = ''
+    if closure_transitions.join.length == 1 
+      closure = "#{closure_transitions.join}*"
+    else
+      closure = "(#{closure_transitions.join('|')})*" unless closure_transitions.empty? 
+    end
 
     incoming = @nodes.map.with_index do |n, i|
       [n.find_all{|tr, st| st == state}[0][0], i] if n.detect{|tr, st| st == state} and i != state
     end.compact
-    #p "Incoming nodes: #{incoming}"
 
     incoming.each do |inc_tr, n|
       @nodes[n].delete(inc_tr)
@@ -53,9 +46,8 @@ class FinitStateMachine
         @nodes[n].delete(existing_tr) if existing_tr
         new_tr = "#{inc_tr}#{closure}#{out_tr}"
         full_tr = [existing_tr, new_tr].compact.join('|')
-        full_tr = "(#{full_tr})"
+        full_tr = "(#{full_tr})" if existing_tr
         @nodes[n][full_tr] = st unless st == state
-      #  p "New transition from #{n}: #{full_tr} => #{st}" unless st == state
       end
     end
 
@@ -67,35 +59,9 @@ class FinitStateMachine
       remove_state(i)
     end
 
-    "^(0|#{@nodes[0].keys.first})*$"
+    "^#{@nodes[0].keys.first}*$"
   end
 end
-
-
-data1 = [ # divisor, input,      expect
-  [4,      44,  true],
-  [1,      5,  true],
-  [1,      1,  true],
-  [1,      0,  true],
-  [1,      11,  true],
-  [2,      22,  true],
-  [2,      16, true],
-  [2,      13, false],
-  [3,      7, false],
-  [5,      65, true],
-  [5,      155, true],
-  [5,      3,  false],
-  [5,      13, false],
-  [7,      140, true],
-  [7,      4793488, true],
-  [10,     20, true],
-  [15,     30, true],
-  [16,     32, true],
-  [17,     34, true],
-#  [18,     18000.to_s(2), true],
-  [3,      "nope",     false],
-  [3,      "3",        false],
-]
 
 
 data2 = []
@@ -106,7 +72,6 @@ for j in 1..18 do
 end
 
 data2.compact!
-
 data2.each{ |n,s,exp|
   regexp = Regexp.new(regex_divisible_by(n))
 
